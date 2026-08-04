c1be3400d5ecac45d13c4ce07d466afdb3d91d2b
rubeshsv <srubeshg@gmail.com>
2026-08-05 00:24:28 +0530

    feat: add day4 FizzBuzz - classic screening problem

---
+++ src/main/java/com/rubeshsv/bible/day4/FizzBuzz.java
@@ -0,0 +1,29 @@
+package com.rubeshsv.bible.day4;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args)
+    {
+        //(divisible by 3 AND 5) → "FizzBuzz" and (divisible by 3) → "Fizz" and (divisible by 5) → "Buzz" and else → the number
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = scanner.nextInt();
+        for(int i=1; i<=num; i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                System.out.println("FizzBuzz");
+            } else if (i%3==0)
+            {
+                System.out.println("Fizz");
+            } else if (i%5==0)
+            {
+                System.out.println("Buzz");
+            }else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
